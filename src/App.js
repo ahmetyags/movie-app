559db69582a517cdb9653a39f8d8488f84d1412f
ahmetyags <ali.ozgur@example.comgit>
2022-11-09 15:54:55 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import AuthContextProvider from "./context/AuthContextProvider";
+import AppRouter from "./router/AppRouter";
 
-function App() {
+const App = () => {
   return (
-    <div>
-
+    <div className="bg-grey-100  dark:bg-[#23242a]">
+      <AuthContextProvider>
+        <AppRouter />
+        <ToastContainer />
+      </AuthContextProvider>
     </div>
   );
-}
+};
 
 export default App;
